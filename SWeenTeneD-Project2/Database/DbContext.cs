22d63cfe6c97ea694476b202b61c2342175cedb8
Hsuan-Yu Lin <o100249243@gmail.com>
2019-10-28 11:41:41 -0500

    db model created working on EF code first

---
+++ SWeenTeneD-Project2/Database/DbContext.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+using System.Data.Common;
+using System.Data.SqlClient;
+using Npgsql.EntityFrameworkCore.PostgreSQL;
+using Npgsql.EntityFrameworkCore;
+using Database;
+
+namespace Database
+{
+    public partial class SWTDbContext : DbContext
+    {
+        public SWTDbContext(){}
+        public SWTDbContext(DbContextOptions<SWTDbContext> options)
+                : base(options) { }
+
+        public virtual DbSet<Customer> Customer { get; set;}
+        public virtual DbSet<Airport> Airport { get; set; }
+        public virtual DbSet<Flight> Flight { get; set; }
+        public virtual DbSet<FlightLocation> FlightLocation { get; set; }
+        public virtual DbSet<FlightTicket> FlightTicket { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder) {
+
+            modelBuilder.Entity<Customer>(entity =>
+            {
+                entity.Property(c => c.CustomerID)
+                      .UseIdentityColumn();
+
+                entity.Property(e => e.FirstName)
+                      .IsRequired()
+                      .HasMaxLength(30);
+
+                entity.Property(e => e.LastName)
+                      .IsRequired()
+                      .HasMaxLength(30);
+
+                entity.Property(e => e.Email)
+                      .IsRequired();
+                      //%@% verified needed
+
+                
+            });
+        }
+    }
+
+}
---
+++ SWeenTeneD-Project2/Database/Flight.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Database
+{
+    public class Flight
+    {
+        //PK
+
+        public int FlightID { get; set; }
+
+        public string Company { get; set; }
+
+        public DateTime DepartureTime { get; set; }
+
+        public DateTime ArrivalTime { get; set; }
+    }
+}
