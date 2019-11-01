b3eefc9d04e9cbbe82d54a58a01fd828c5b30d54
Topsunkist <arizonadub12@gmail.com>
2019-11-01 09:21:13 -0500

    Changed the route name for the controllers and trying to implement the customer controller by testing in POSTMAN

--- SWeenTeneD-Project2/API/Controllers/CustomerController.cs
+++ SWeenTeneD-Project2/API/Controllers/CustomerController.cs
@@ -4,13 +4,17 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Database;
 
 namespace API.Controllers
 {
+   
     [Route("api/[controller]")]
     [ApiController]
     public class CustomerController : ControllerBase
     {
+
+        private static Repo repo;
         //REPO goes here
 
         //private readonly CustomerL
@@ -19,40 +23,62 @@ namespace API.Controllers
         [HttpGet]
         public IEnumerable<string> Get()
         {
-            return new string[] { "value1", "value2" };
+            return new string[] { "Customer", "GET GET" };
         }
 
-        /*
-         * [HttpGet]
-         * public IActionResult GetAllCustomers(info){
-         *      IEnumerable<Customer> customers = Repo.ReadCustomerList(info);
-         *      IEnumerable<APICustomer> apiCustomer = customers.Select( c=> new APICustomer
-         *      {
-         *          //From APIModel = Logic
-         *          CustomerID = c.CustomerID,
-         *          FirstName = c.FirstName,
-         *          LastName = c.LastName,
-         *          Email = c.Email,
-         *          Password = c.Password
-         *  
-         *      
-         *      });
-         *      return apiCustomer;
-         * }
-         */
+
+        //[HttpGet]
+        //public IActionResult GetAllCustomers(info){
+        //     IEnumerable<Logic.Customer> customers = Repo.ReadCustomerList(info);
+        //     IEnumerable<API.Models.APICustomer> apiCustomer = customers.Select( c=> new API.Models.APICustomer
+        //     {
+        //         //From APIModel = Logic
+        //         CustomerID = c.CustomerID,
+        //         FirstName = c.FirstName,
+        //       LastName = c.LastName,
+        //         Email = c.Email,
+        //         Password = c.Password
+
+
+        //     });
+        //     return apiCustomer;
+        //}
+
 
 
         // GET: api/Customer/5
-        [HttpGet("{id}", Name = "Get")]
+        [HttpGet("{id}", Name = "GetCustomer")]
         public string Get(int id)
         {
             return "value";
         }
 
         // POST: api/Customer
-        [HttpPost]
-        public void Post([FromBody] string value)
-        {
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+        
+          [HttpPost]
+          public ActionResult Create(Logic.Customer customer){
+
+            Logic.Customer cus = new Logic.Customer
+            {
+                CustomerID = customer.CustomerID,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                Email = customer.Email,
+                Password = customer.Password
+
+            };
+
+            var newID = customer.CustomerID;
+
+            string a = repo.CreateCustomer(cus);
+
+            return CreatedAtRoute("Get", new { Id = newID}, customer);
+            //return Ok();
+
         }
 
         // PUT: api/Customer/5
