fee724cc91c8314fc94631314bced216a24163c6
Topsunkist <arizonadub12@gmail.com>
2019-11-02 15:44:08 -0500

    Added necessary API models, completed Airport controller. GET and POST works, but PUT and DELETE don't seem to be.  Will fix.

--- SWeenTeneD-Project2/API/Controllers/AirportController.cs
+++ SWeenTeneD-Project2/API/Controllers/AirportController.cs
@@ -4,13 +4,20 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Logic;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")]
+    [Route("~/api/[controller]")]
     [ApiController]
     public class AirportController : ControllerBase
     {
+        private static IRepo iRepo;
+        public AirportController(IRepo repo)
+        {
+            iRepo = repo; 
+        }
+
         // GET: api/Airport
         [HttpGet]
         public IEnumerable<string> Get()
@@ -18,29 +25,76 @@ namespace API.Controllers
             return new string[] { "value1", "value2" };
         }
 
-        // GET: api/Airport/5
-        [HttpGet("{id}", Name = "GetAirport")]
-        public string Get(int id)
+        // GET: api/Airport/Airport's name
+        [HttpGet("{name}", Name = "GetAirport")]
+        public IEnumerable<API.Models.APIAirport>GetByName(string name)
         {
-            return "value";
+            Logic.Airport LAir = new Logic.Airport();
+            LAir.Name = name;
+            IEnumerable<Logic.Airport> airports = iRepo.ReadAirportList(LAir);
+            IEnumerable<API.Models.APIAirport> apiAirport = airports.Select(a => new API.Models.APIAirport
+            {
+                //APIModel = Logic
+                AirportID = a.AirportID,
+                Name = a.Name,
+                Location = a.Location,
+                Weather = a.Weather
+
+            });
+
+            return apiAirport;
         }
 
         // POST: api/Airport
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult Post([FromBody, Bind("Name, Location, Weather")] API.Models.APIAirport airport)
         {
+            Logic.Airport air = new Logic.Airport
+            {
+                AirportID = airport.AirportID,
+                Name = airport.Name,
+                Location = airport.Location,
+                Weather = airport.Weather
+            };
+
+            iRepo.CreateAirport(air);
+
+            return CreatedAtRoute("GetAirport", new {Name = air.Name}, airport);
         }
 
-        // PUT: api/Airport/5
+        // PUT: api/Airport/Name of airport you want to edit
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public IActionResult Put(string id, [FromBody] API.Models.APIAirport Aairport)
         {
+            Logic.Airport air = new Logic.Airport();
+            air.Name = id;
+
+            IEnumerable<Logic.Airport> Lairports = iRepo.ReadAirportList(air);
+
+            //Need exception handling here, maybe implement in repo?
+            Logic.Airport newAir = new Logic.Airport
+            {
+                AirportID = Aairport.AirportID,
+                Name = Aairport.Name,
+                Location = Aairport.Location,
+                Weather = Aairport.Weather
+            };
+
+            iRepo.UpdateAirport(newAir);
+            return Ok();
         }
 
-        // DELETE: api/ApiWithActions/5
+        // DELETE: api/airport/AirportID
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public IActionResult Delete(int id)
         {
+            Logic.Airport air = new Logic.Airport();
+            air.AirportID = id;
+
+            IEnumerable<Logic.Airport> Lairports = iRepo.ReadAirportList(air);
+            iRepo.DeleteAirport(air);
+
+            return Ok();
         }
     }
 }
---
+++ SWeenTeneD-Project2/API/Models/APIAirport.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace API.Models
+{
+    public class APIAirport
+    {
+        public int AirportID { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        public string Weather { get; set; }
+    }
+}
