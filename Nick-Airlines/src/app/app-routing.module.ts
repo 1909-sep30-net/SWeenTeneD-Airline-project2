d2e2e0b1cc5d80007b5cfe839ccb57d9b102c6ff
Domanik <domaniklavonn@gmail.com>
2019-11-02 10:13:40 -0500

    Fixed routing in Nicklines and added some design to login and register page

--- Nick-Airlines/src/app/app-routing.module.ts
+++ Nick-Airlines/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {RegisterComponent } from './register/register.component';
+import {LoginComponent } from './login/login.component';
+import {PagenotfoundComponent } from './pagenotfound/pagenotfound.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+{path:'', component:RegisterComponent },
+{path:'login',component: LoginComponent },
+{path:'pagenotfound', component: PagenotfoundComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- Nick-Airlines/src/app/pagenotfound/pagenotfound.component.ts
+++ Nick-Airlines/src/app/pagenotfound/pagenotfound.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./pagenotfound.component.css']
 })
 export class PagenotfoundComponent implements OnInit {
-
+  title:string = "Page Not Found";
   constructor() { }
 
   ngOnInit() {
