597e040cf5f20f31aa424b03f77ffa668f2709a8
Domanik <40034696+Domljohns@users.noreply.github.com>
2019-11-04 11:04:35 -0600

    Domanikj (#16)
    * Added Angular and VSCode to gitignore file
    
    * Created Angular project
    
    * Created login and register component
    
    * Added Login and Register to my app-routing.module
    
    * Seperated register from app root
    
    * Added home comp
    
    * Added register page design! I still have to change form around for Angular Template!!
    
    * Added this as a test to check whether I did something wrong with my routesgit add .
    
    * Fixed routing in Nicklines and added some design to login and register page
    
    * Updated right div and edited .twitter with .github
    
    * Updated PageNotFound! forgot to put wildcard symbols
    
    * Added own module to export and import Angular Material and created header C for customer views

---
+++ Nick-Airlines/src/app/material/material.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import {MatButtonModule,
+   MatButtonToggleModule, 
+   MatIconModule, 
+   MatToolbarModule,
+    MatSidenavModule} from '@angular/material';
+
+
+const Material = [
+  MatButtonModule,
+  MatButtonToggleModule,
+  MatIconModule,
+  MatToolbarModule,
+  MatSidenavModule
+]
+
+@NgModule({
+  imports: [Material],
+  exports: [Material]
+})
+export class MaterialModule { }
