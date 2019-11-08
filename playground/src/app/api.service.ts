b2eb21b2172448c8827c21d53c832c58ba6f5822
Wesley Tang <wesley.tang25@gmail.com>
2019-11-07 23:28:25 -0600

    close to working? need DB Url

--- playground/src/app/api.service.ts
+++ playground/src/app/api.service.ts
@@ -1,11 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { environment } from 'src/environments/environment';
+import { environment } from '../environments/environment';
+import Customer from './customer';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
+
+  getItems(): Promise<Customer[]> {
+    const url = `${environment.BaseUrl}/api/customer/0`;
+    return this.httpClient.get<Customer[]>(url).toPromise();
+  }
 }
