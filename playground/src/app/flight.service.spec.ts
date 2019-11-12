935547057d8cc0999066bc44fe7416a127a15528
Wesley Tang <wesley.tang25@gmail.com>
2019-11-12 01:26:28 -0600

    a

--- playground/src/app/flight.service.spec.ts
+++ playground/src/app/flight.service.spec.ts
@@ -1,12 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 
-import { flightService } from './flight.service';
+import { BookflightService } from './Flight.service';
 
 describe('BookflightService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: flightService = TestBed.get(flightService);
+    const service: BookflightService = TestBed.get(BookflightService);
     expect(service).toBeTruthy();
   });
 });
--- playground/src/app/flight.service.ts
+++ playground/src/app/flight.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../environments/environment.prod';
-import Flight from  './flight';
+import Flight from  './Flight';
 import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class flightService {
+export class BookflightService {
 
   constructor(private httpClient: HttpClient) { }
 
-  url = `${environment.BaseUrl}/api/airport`;
+  url = `${environment.BaseUrl}/api/flight`;
 
   getItems(): Observable<Flight[]> {
     
